975f6107193197f4ba462b053ce7f12e9ee5e385
youssef <youssefmaghzaz@gmail.com>
2020-03-28 15:33:28 +0100

    setup project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from react</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));

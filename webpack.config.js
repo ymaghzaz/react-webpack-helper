1760b0a5afd63a3ddc23c5204f8218c0c656ca31
youssef <youssefmaghzaz@gmail.com>
2020-03-28 22:05:20 +0100

    webpack add chunk

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,19 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
-  },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 5000
+  },
+  output: {
+    filename: "[hash].[name].js",
+    chunkFilename: "[hash].[name].js"
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all"
+    }
   },
   module: {
     rules: [

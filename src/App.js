6f9bb6938ec7120dfbcdae3899053a895be30d50
youssef <youssefmaghzaz@gmail.com>
2020-03-28 17:03:35 +0100

    add counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello from react webpack babel </h1>
-      </div>
-    );
+const styles = {
+  container: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    margin: "1vw",
+    width: "5vw",
+    height: "4vh",
+    fontSize: "large"
   }
-}
+};
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <h1>Hello from react webpack babel </h1>
+      <div style={styles.container}>
+        <h3> counter value : {counter}</h3>
+        <button
+          style={styles.button}
+          onClick={() => {
+            setCounter(counter + 1);
+          }}
+        >
+          + 1
+        </button>
+        <button
+          style={styles.button}
+          onClick={() => {
+            setCounter(counter - 1);
+          }}
+        >
+          {" "}
+          - 1
+        </button>
+      </div>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
